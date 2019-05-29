d164de21ccf47b1710d847f4c143f3d3224dbe06
Dennis <dennis.basyrov@gmail.com>
2019-05-29 03:54:47 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,8 +1,8 @@
 ESPERANTO_ALPHABET = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
-  arr.sort_by do |a| 
-    i =  a.split("")
+  arr.sort_by do |ele| 
+    i =  ele.split("")
     i.map do |char| 
     ESPERANTO_ALPHABET.index(char)
     end
